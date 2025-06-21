6c20f33f35587e49a6dc2f644fd4c2c86a1a2db0
mreagleowl <90679212+mreagleowl@users.noreply.github.com>
2025-06-21 14:19:31 +0300

    1

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./styles/tailwind.css";
+
+function App() {
+  return <div style={{color:'red', fontSize:'40px'}}>!!! REACT ЖИВОЙ !!!</div>;
+}
 
 createRoot(document.getElementById("root")).render(<App />);
