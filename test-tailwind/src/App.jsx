7ecac67f459abad288acdcb1ad8a5eaacf1e4ab1
mreagleowl <90679212+mreagleowl@users.noreply.github.com>
2025-06-22 11:36:31 +0300

    1

--- test-tailwind/src/App.jsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="bg-blue-700 text-white p-10 text-3xl rounded-xl border-4 border-red-500">
-      Якщо цей текст не білий на синьому фоні з червоною рамкою — Tailwind НЕ працює!
-    </div>
-  );
-}
--- test-tailwind/src/test.js
+++
@@ -1,2 +0,0 @@
-// test.js
-export const dummy = '<div class="bg-green-500 text-white p-4">Тест Tailwind!</div>';
