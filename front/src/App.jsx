fe3679f0ac5bf48805f73215c26ed530c7815539
mreagleowl <90679212+mreagleowl@users.noreply.github.com>
2025-06-22 13:15:16 +0300

    Update via API

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,7 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Themes from "./pages/Themes";
 import Pib from "./pages/Pib";
-// Импорт других страниц позже (Test, Result, Admin)
+import Test from "./pages/Test";
+import Result from "./pages/Result";
+import Admin from "./pages/Admin";
 
 export default function App() {
   return (
@@ -11,9 +13,9 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/themes" element={<Themes />} />
         <Route path="/pib" element={<Pib />} />
-        {/* <Route path="/test" element={<Test />} /> */}
-        {/* <Route path="/result" element={<Result />} /> */}
-        {/* <Route path="/admin" element={<Admin />} /> */}
+        <Route path="/test" element={<Test />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
   );
