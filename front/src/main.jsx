6475f93f6c9fd0ba41e311530728079b9857dd77
mreagleowl <90679212+mreagleowl@users.noreply.github.com>
2025-06-22 11:51:53 +0300

    Update via API

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,6 +1,10 @@
-import "./index.css";
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
